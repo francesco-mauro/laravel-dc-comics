434590cc6bfacbbcf701b4e9ff24df2ead956a0e
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-15 18:35:49 +0200

    add: working ComicsTableSeeder

--- database/seeders/ComicsTableSeeder.php
+++ database/seeders/ComicsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comic;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,19 @@ class ComicsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $comics = config('comics');
+        dd($comics);
+
+        foreach ($comics as $comic) {
+            Comic::create([
+                'title' => $comic['title'],
+                'description' => $comic['description'],
+                'thumb' => $comic['thumb'],
+                'price' => $comic['price'],
+                'series' => $comic['series'],
+                'sale_date' => $comic['sale_date'],
+                'type' => $comic['type'],
+            ]);
+        }
     }
 }
